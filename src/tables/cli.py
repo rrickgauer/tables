96c43d2ed8bad8247aa9ffcf2042ae4dde5b019f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-05-03 13:37:16 -0500

    create the new cli args

--- src/tables/cli.py
+++ src/tables/cli.py
@@ -1,13 +1,22 @@
 """
 ************************************************************************************
 
-This is class for the command line argument parser.
+Command line arguments parser.
+
+The new cli design has 4 commands:
+    - list: prints out all the stored connections
+    - add: create/save a new connection
+    - view: view a saved connection
+    - delete: delete a saved connection
+
+Each command has its own seperate set of flags/arguments.
 
 ***************************************************************************************
 """
 
 from __future__ import annotations
 import argparse
+from tables.domain.enums import CliCommands
 
 class CliArgs:
 
@@ -16,6 +25,7 @@ class CliArgs:
 
         self._parser = argparse.ArgumentParser(description="Display your mysql database table schemas")
         self._args = None
+        self._subparser = None
 
     def parse(self):
         """Parse the command line arguments"""
@@ -25,9 +35,33 @@ class CliArgs:
 
     def _add_arguments(self):
         """Add all the arguments to the parser"""
+
+        # create a command subparser
+        self._subparser = self._parser.add_subparsers(dest='command')
+
+        # add each of the command flag arguments
+        self._add_subparser_list()
         
-        # self._parser.add_argument('-d', '--development', action="store_false", help=ArgumentDescriptions.DEVELOPMENT_FLAG)
-        pass
+
+    def _add_subparser_list(self):
+        """Add the cli flag arguments for the list command."""
+
+        sub_parser = self._subparser.add_parser(CliCommands.LIST.value)
+        sub_parser.add_argument('--username', type=str)
+
+
+    @property
+    def command(self) -> CliCommands:
+        """Get the command."""
+
+        try:
+            command = CliCommands(self._args.command)
+        except ValueError:
+            command = CliCommands.LIST
+
+        return command
+
+        
 
     
     
